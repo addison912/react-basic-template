c003b29121e757f569f09e29bcd8efd71501c96f
Addison <addisondrewmoore@gmail.com>
2020-02-29 20:55:12 -0800

    added css to index file and created a container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { createHistory, LocationProvider, Router } from "@reach/router";
+import createHashSource from "hash-source";
+
+// Components
+import Home from "./containers/Home";
+
+let source = createHashSource();
+let history = createHistory(source);
 
 class App extends React.Component {
   render() {
     return (
       <React.StrictMode>
-        <div className="App">Hello World</div>;
+        <LocationProvider history={history}>
+          <div className="App" path="/">
+            <Router>
+              <Home path="/" />
+            </Router>
+          </div>
+        </LocationProvider>
       </React.StrictMode>
     );
   }
