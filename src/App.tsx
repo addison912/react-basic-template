4b25bff69911061848ff8c12a9f8921c4813bd68
Addison Moore <addisondrewmoore@gmail.com>
2021-03-15 21:47:30 -0700

    disabled suspense

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React, { lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Header } from "./components/Header";
-import OtherRoute from "./components/OtherRoute";
 
 // Components
-const Home = lazy(() => import("./components/Home"));
+// const Home = lazy(() => import("./components/Home"));
+import OtherRoute from "./components/OtherRoute";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -13,16 +14,16 @@ const App = () => {
       <div className="App">
         <Router>
           <Header />
-          <Suspense fallback={<h1>loading ...</h1>}>
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route exact path="/other">
-                <OtherRoute />
-              </Route>
-            </Switch>
-          </Suspense>
+          {/* <Suspense fallback={<h1>loading ...</h1>}> */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/other">
+              <OtherRoute />
+            </Route>
+          </Switch>
+          {/* </Suspense> */}
         </Router>
       </div>
     </React.StrictMode>
