099f46baba6460b1bf5530e06b28432defe2c537
Addison Moore <addisondrewmoore@gmail.com>
2021-04-08 14:07:09 -0700

    no change

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -9,6 +9,7 @@ class ErrorBoundary extends Component {
     return { hasError: true };
   }
   componentDidCatch(error, info) {
+    // eslint-disable-next-line no-console
     console.error("ErrorBoundary caught an error", error, info);
   }
   componentDidUpdate() {
